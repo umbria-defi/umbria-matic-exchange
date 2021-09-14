2813d4ccb2c7119dc51dde24bf684b2ea64c6e97
test1 <aarong11@gmail.com>
2021-09-14 14:46:16 +0100

    Changed title to reflect which info site

--- src/components/AccountDetails/index.tsx
+++ src/components/AccountDetails/index.tsx
@@ -358,7 +358,7 @@ export default function AccountDetails({
                             href={chainId && getEtherscanLink(chainId, ENSName, 'address')}
                           >
                             <LinkIcon size={16} />
-                            <span style={{ marginLeft: '4px' }}>View on Polygon Explorer</span>
+                            <span style={{ marginLeft: '4px' }}>View on Umbria Analytics</span>
                           </AddressLink>
                         )}
                       </div>
@@ -380,7 +380,7 @@ export default function AccountDetails({
                             href={getEtherscanLink(chainId, account, 'address')}
                           >
                             <LinkIcon size={16} />
-                            <span style={{ marginLeft: '4px' }}>View on Polygon Explorer</span>
+                            <span style={{ marginLeft: '4px' }}>View on Umbria Analytics</span>
                           </AddressLink>
                         )}
                       </div>
